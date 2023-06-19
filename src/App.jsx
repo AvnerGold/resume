2675c431b14359377e20a4950574116193ba37dc
AvnerGold <avnergoldberg@hotmail.fr>
2023-06-19 19:26:28 +0300

    Nav created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Nav from './component/Nav'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        
+          <Nav/>
           <Routes>
           <Route path='/' element={<Home />} />
           
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,7 +1,55 @@
 import React from 'react'
+import myImage  from '../assets/images/nav-img/me.jpg'
+import '../styles/nav.css'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <>
+    <div style={{ width: '100vw' }} className="profile">
+
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+   <img src={myImage} alt="MY Image" style={{ maxWidth: '90px', maxHeight: '150px', objectFit: 'contain', padding: '10px', borderRadius: '30%' }} />
+  <p>Avner <br /> Goldberg</p>
+
+    </div>
+
+
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" ></span>
+    </button>
+
+    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link </a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu " aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+     
+    </div>
+  </nav>
+</div>
+
+
+
+    </>
+   
   )
 }
