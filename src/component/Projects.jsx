4f387787110be7575f11c58ba02aed05a722a291
AvnerGold <avnergoldberg@hotmail.fr>
2023-06-23 18:31:20 +0300

    rectify text

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -5,12 +5,19 @@ export default function Projects({ name, header, url, img, text }) {
     console.log(name, header, url, img, text);
   
     return (
-      <div className="card">
-        <img src={img} className="card-img-top" alt={name} />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{text}</p>
-          <a href={url} className="btn btn-primary">Go to project</a>
+        <div className="card">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={img} className="card-img" alt={name} />
+            <div className='neon-line'></div>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h5 className="card-title">{name} <br /> {header}</h5>
+              <p className="card-text">{text}</p>
+              <a href={url} className="btn btn-primary">Go to project</a>
+            </div>
+          </div>
         </div>
       </div>
     );
